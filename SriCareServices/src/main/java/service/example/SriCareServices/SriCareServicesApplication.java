6033a3e094af1744e0edf82a6ec84d06125129f6
Tharindi Senadeera <tia.tharindi@gmail.com>
2023-09-19 15:19:43 +0530

    commit 1

---
+++ SriCareServices/src/main/java/service/example/SriCareServices/SriCareServicesApplication.java
@@ -0,0 +1,13 @@
+package service.example.SriCareServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SriCareServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SriCareServicesApplication.class, args);
+	}
+
+}
---
+++ SriCareServices/src/test/java/service/example/SriCareServices/SriCareServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package service.example.SriCareServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SriCareServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
